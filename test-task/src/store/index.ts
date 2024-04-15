eb91de40f6dc2b857987f1c8b2e0ef4029a0c88a
HAVENBURN <burnsday@mail.ru>
2024-04-15 11:52:25 +0300

    added iconChange Function

--- test-task/src/store/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import apiReducer from './reducer/iconsReducer';
-
-export const rootReducer = combineReducers({
-    Api: apiReducer,
-})
-
-export type rootState = ReturnType<typeof rootReducer>
